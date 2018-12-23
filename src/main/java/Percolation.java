d421e8bed51554b9d07bce299ded4b9ef2b9c98e
Luka_Giorgadze <luka.giorgadze94@gmail.com>
2018-12-23 12:01:41 +0600

    Implemented the percolation logic using the weighted union find algo

--- src/main/java/Percolation.java
+++ src/main/java/Percolation.java
@@ -4,13 +4,14 @@ public class Percolation {
     private int n;
     private int[][] grid;
     private WeightedQuickUnionUF union;
+    private int numberOfOpenSites;
 
     public Percolation(int n) { // create n-by-n grid, with all sites blocked
         if (n <= 0) {
             throw new IllegalArgumentException();
         }
         this.n = n;
-        union = new WeightedQuickUnionUF(n ^ 2);
+        union = new WeightedQuickUnionUF(n * n);
         grid = new int[n][n];
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < n; j++) {
@@ -22,38 +23,81 @@ public class Percolation {
     public void open(int row, int col) {  // open site (row, col) if it is not open already
         verifyInputIsInAllowedRange(row);
         verifyInputIsInAllowedRange(col);
+        grid[row - 1][col - 1] = 0;
+        uniteOpenSites(row, col);
+        numberOfOpenSites++;
+    }
 
+    private void uniteOpenSites(int x, int y) {
+        x = x - 1;
+        y = y - 1;
+        if (!(x - 1 < 0)) {
+            if (isOpenWithoutOffset(x - 1, y)) {
+                union.union(xyTo1d(x, y), xyTo1d(x - 1, y));
+            }
+        }
+        if (!(y - 1 < 0)) {
+            if (isOpenWithoutOffset(x, y - 1)) {
+                union.union(xyTo1d(x, y), xyTo1d(x, y - 1));
+            }
+        }
+        if (!(x + 1 >= n)) {
+            if (isOpenWithoutOffset(x + 1, y)) {
+                union.union(xyTo1d(x, y), xyTo1d(x + 1, y));
+            }
+        }
+        if (!(y + 1 >= n)) {
+            if (isOpenWithoutOffset(x, y + 1)) {
+                union.union(xyTo1d(x, y), xyTo1d(x, y + 1));
+            }
+        }
+    }
+
+    private boolean isOpenWithoutOffset(int row, int col) { // is site (row, col) open?
+        return grid[row][col] == 0;
     }
 
     public boolean isOpen(int row, int col) { // is site (row, col) open?
         verifyInputIsInAllowedRange(row);
         verifyInputIsInAllowedRange(col);
-
-        return false;
+        return grid[row - 1][col - 1] == 0;
     }
 
     public boolean isFull(int row, int col) { // is site (row, col) full?
         verifyInputIsInAllowedRange(row);
         verifyInputIsInAllowedRange(col);
-
-        return false;
+        boolean isFull = false;
+        for (int i = 0; i < n; i++) {
+            if (union.connected(xyTo1d(row - 1, col - 1), xyTo1d(0, i))) {
+                isFull = true;
+                break;
+            }
+        }
+        return isFull;
     }
 
     public int numberOfOpenSites() { // number of open sites
-        return 0;
+        return numberOfOpenSites;
     }
 
     public boolean percolates() { // does the system percolate?
-        return false;
-    }
-
-    public static void main(String[] args) {
-
+        boolean percolates = false;
+        for (int i = 1; i <= n; i++) {
+            if (isFull(n, i)) {
+                percolates = true;
+                break;
+            }
+        }
+        return percolates;
     }
 
     private void verifyInputIsInAllowedRange(int dimension) {
         if (dimension > n || dimension < 1) {
             throw new IllegalArgumentException();
         }
+    }
+
+    private int xyTo1d(int x, int y) {
+        return x * 10 + y;
     }
 }
--- src/main/java/PercolationStats.java
+++ src/main/java/PercolationStats.java
@@ -1,3 +1,4 @@
+import edu.princeton.cs.algs4.StdRandom;
 import edu.princeton.cs.algs4.StdStats;
 import edu.princeton.cs.algs4.Stopwatch;
 
@@ -34,14 +35,22 @@ public class PercolationStats {
     public static void main(String[] args) { // test client (described below)
         int n = Integer.parseInt(args[0]);
         int trials = Integer.parseInt(args[1]);
+
         results = new double[n];
         for (int i = 0; i < trials; i++) {
             Stopwatch stopwatch = new Stopwatch();
             Percolation percolation = new Percolation(n);
             while (!percolation.percolates()) {
-
+                int x = StdRandom.uniform(1, n + 1);
+                int y = StdRandom.uniform(1, n + 1);
+                if (!percolation.isOpen(x, y)) {
+                    percolation.open(x, y);
+                }
             }
             results[i] = stopwatch.elapsedTime();
         }
+        System.out.println("mead: " + StdStats.mean(results));
+        System.out.println("stddev: " +  StdStats.stddev(results));
+        System.out.println("95% confidence interval: [" + StdStats.min(results) + ", " + StdStats.max(results) + "]");
     }
 }
